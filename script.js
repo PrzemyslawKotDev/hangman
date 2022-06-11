d1ea1e8ce27b4ec32280bc10e28b24e4082bc301
Przemysław Kot <pkot@int.pl>
2022-06-11 09:49:59 +0200

    fully functional
    added regex to check password, game is working

--- script.js
+++ script.js
@@ -22,70 +22,95 @@ function buildHangmanGenerator (containerID){
 
                 hangman.append(input);
 
-          const buttonGenerate = document.createElement('button');
-                buttonGenerate.setAttribute('onclick', 'generateHangman()');
-                buttonGenerate.setAttribute('target', 'hangmanGame');
-                buttonGenerate.setAttribute('id', 'hangmanButtonGenerate');
-                buttonGenerate.innerText = 'Start game';
+          const chancesBoard = document.createElement('div');
+                chancesBoard.setAttribute('target', 'hangmanGame');
+                chancesBoard.setAttribute('id', 'chancesBoard');
 
-                hangman.append(buttonGenerate);
+                hangman.append(chancesBoard);
+
+              const chances4 = document.createElement('button');
+                    chances4.setAttribute('onclick', 'generateHangman(4)');
+                    chances4.setAttribute('target', 'hangmanGame');
+                    chances4.classList.add('chancesButton');
+                    chances4.innerText = '4 chances';
+
+                    chancesBoard.append(chances4);
+
+              const chances7 = document.createElement('button');
+                    chances7.setAttribute('onclick', 'generateHangman(7)');
+                    chances7.setAttribute('target', 'hangmanGame');
+                    chances7.classList.add('chancesButton');
+                    chances7.innerText = '7 chances';
+
+                    chancesBoard.append(chances7);
+
+              const chances10 = document.createElement('button');
+                    chances10.setAttribute('onclick', 'generateHangman(10)');
+                    chances10.setAttribute('target', 'hangmanGame');
+                    chances10.classList.add('chancesButton');
+                    chances10.innerText = '10 chances';
+
+                    chancesBoard.append(chances10);
 
 }
-
-function generateHangman(){
+let hangmanInstance;
+function generateHangman(chances){
+    let regex = /^[A-Za-z\s]*$/;
     const password = document.getElementById('hangmanInput');
-    //DODAĆ WARUNEK ŻE PASSWORD NIE MOŻE BYĆ PUSTY
-    const hangmanInstance = new Hangman (password.value, 5);
-          hangmanInstance.getPuzzle(password);
+    if(regex.test(password.value)){
+
+    hangmanInstance = new Hangman (password.value, chances);
+    hangmanInstance.getPuzzle(password);
+
+const hangman = document.getElementById('hangmanContainer');
+      hangman.classList.add('blackBackground');
+      hangman.innerText = '';
+
+const hangmanPassword = document.createElement('div');
+      hangmanPassword.setAttribute('target', 'hangmanGame');
+      hangmanPassword.setAttribute('id', 'hangmanPassword');
+      hangmanPassword.innerText = `${hangmanInstance.puzzled.join('')}`;
+
+      hangman.append(hangmanPassword);
+
+const gameContent = document.createElement('div');
+      gameContent.setAttribute('target', 'hangmanGame');
+      gameContent.setAttribute('id', 'gameContent');
+
+      hangman.append(gameContent);
+
+    const hangmanImageContainer = document.createElement('div');
+          hangmanImageContainer.setAttribute('target', 'hangmanGame');
+          hangmanImageContainer.setAttribute('id', 'hangmanImageContainer');
+          
+          gameContent.append(hangmanImageContainer);
+
+        const hangmanImage = document.createElement('div');
+              hangmanImage.setAttribute('target', 'hangmanGame');
+              hangmanImage.setAttribute('id', 'hangmanImage');
+              //hangmanImage.setAttribute('src', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRNDun5A4cMkQdU8tHBHRmQLexETpTvutgW6w&usqp=CAU');
+              hangmanImage.innerText = `${hangmanInstance.guess}`;
+          
+              hangmanImageContainer.append(hangmanImage);
 
 
-  const hangman = document.getElementById('hangmanContainer');
-        hangman.classList.add('blackBackground');
-        hangman.innerText = '';
+    const hangmanBoard = document.createElement('div');
+          hangmanBoard.setAttribute('target', 'hangmanGame');
+          hangmanBoard.setAttribute('id', 'hangmanBoard');
 
-  const hangmanPassword = document.createElement('div');
-        hangmanPassword.setAttribute('target', 'hangmanGame');
-        hangmanPassword.setAttribute('id', 'hangmanPassword');
-        hangmanPassword.innerText = `${hangmanInstance.puzzled.join('')}`;
+          gameContent.append(hangmanBoard);
 
-        hangman.append(hangmanPassword);
+          generateBoard();
 
-  const gameContent = document.createElement('div');
-        gameContent.setAttribute('target', 'hangmanGame');
-        gameContent.setAttribute('id', 'gameContent');
+    const resetButton = document.createElement('button');
+          resetButton.setAttribute('target', 'hangmanGame');
+          resetButton.setAttribute('id', 'resetButton');
+          resetButton.setAttribute('onclick', 'resetHangman()');
+          resetButton.innerText = 'RESET';
 
-        hangman.append(gameContent);
+          hangman.append(resetButton);
 
-      const hangmanImageContainer = document.createElement('div');
-            hangmanImageContainer.setAttribute('target', 'hangmanGame');
-            hangmanImageContainer.setAttribute('id', 'hangmanImageContainer');
-            
-            gameContent.append(hangmanImageContainer);
-
-          const hangmanImage = document.createElement('img');
-                hangmanImage.setAttribute('target', 'hangmanGame');
-                hangmanImage.setAttribute('id', 'hangmanImage');
-                hangmanImage.setAttribute('src', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRNDun5A4cMkQdU8tHBHRmQLexETpTvutgW6w&usqp=CAU');
-            
-                hangmanImageContainer.append(hangmanImage);
-
-
-      const hangmanBoard = document.createElement('div');
-            hangmanBoard.setAttribute('target', 'hangmanGame');
-            hangmanBoard.setAttribute('id', 'hangmanBoard');
-
-            gameContent.append(hangmanBoard);
-
-            generateBoard();
-
-      const resetButton = document.createElement('button');
-            resetButton.setAttribute('target', 'hangmanGame');
-            resetButton.setAttribute('id', 'resetButton');
-            resetButton.setAttribute('onclick', 'resetHangman()');
-            resetButton.innerText = 'RESET';
-
-            hangman.append(resetButton);
-
+    } else {alert('Nieprawidłowe znaki!')}
 }
 
 function generateBoard(){
@@ -98,7 +123,7 @@ function generateBoard(){
 
       const letterButton = document.createElement('button');
             letterButton.setAttribute('target', 'hangmanGame');
-            letterButton.setAttribute('id', `hangmanGame${letter}`);
+            letterButton.setAttribute('onclick', `checkLetter('${letter}')`);
             letterButton.classList.add('letter')
             letterButton.innerText = letter;
 
@@ -111,10 +136,17 @@ function resetHangman(){
     buildHangmanGenerator('container')
 }
 
+function checkLetter(letter){
+    hangmanInstance.guessLetter(letter);
+let hangmanPassword = document.getElementById('hangmanPassword');
+    hangmanPassword.innerText = `${hangmanInstance.puzzled.join('')}`;
+let hangmanImage = document.getElementById('hangmanImage');
+    hangmanImage.innerText = `${hangmanInstance.guess}`;
+}
+
 const Hangman = function (word, guess) {
-    this.word = word.toLowerCase().split('');
+    this.word = word.toUpperCase().split('');
     this.guess = guess;
-    this.letters = [];
     this.puzzled = [];
 }
 
@@ -126,14 +158,20 @@ Hangman.prototype.getPuzzle = function (){
     })
 }
 Hangman.prototype.guessLetter = function (guessedLetter){
+    let lettersIndex = [];
     this.word.forEach((letter, index)=>{
-        if(letter === guessedLetter) {lettersIndex.push(index)} 
+        if(letter === guessedLetter) {lettersIndex.push(index)}
     });
-    for(let i=0; i<lettersIndex.length; i++){
-        this.puzzled.splice(lettersIndex[i], 1, guessedLetter);
+    if(lettersIndex.length < 1){
+        this.guess = this.guess-1
+    } else {
+        for(let i=0; i<lettersIndex.length; i++){
+            this.puzzled.splice(lettersIndex[i], 1, guessedLetter);
+        }
     }
-    console.log(this.puzzled);
+    if(this.puzzled.join('') === this.word.join('')) {
+        setTimeout(()=>alert('WIN!'), 50)
+    }
 }
-//console.log(game1.guessLetter('t'))
 
 window.onload = buildHangmanGenerator('container');
